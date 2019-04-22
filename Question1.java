5483552723d607efb207a87b93d2e80f42f2d378
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-04-22 16:39:25 +0900

    配列

--- Question1.java
+++ Question1.java
@@ -1,22 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-class Question1{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+import java.lang.NumberFormatException;
+
+class Question1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("®”2‚Â‚ğ‹ó”’‚ğ‹²‚ñ‚Å-10000‚©‚ç10000‚Å“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
-        String input =br.readLine();
-        String[] param=input.split(" ");
+        String input = br.readLine();
+        String[] param = input.split(" ");
 
-        short x=Short.parseShort(param[0]); 
-        short y=Short.parseShort(param[1]);
+        short x = 0;
+        short y = 0;
 
-        if(x<=10000 && x>=-10000){
-            if(y<=10000 && y>=-10000){
-                System.out.println(x+y);
-            }
-        }else{
+        try {
+            x = Short.parseShort(param[0]);
+            y = Short.parseShort(param[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("-10000‚©‚ç10000‚Å“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+            return;
+        }
+
+        if (x <= 10000 && x >= -10000 && y <= 10000 && y >= -10000) {
+            System.out.println(x + y);
+        } else {
             System.out.println("-10000‚©‚ç10000‚Å“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
         }
     }
