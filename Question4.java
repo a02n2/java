ebf57d0657315eed87b0743d334bc54943dbe2e0
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-05-07 15:35:47 +0900

    Update Question4.java
    "("に対応する")"が正確ではなかったのでcountを使用し修正

--- Question4.java
+++ Question4.java
@@ -17,22 +17,17 @@ public class Question4 {
 
     public static int calc(String[] param, int start, int end) {
 
-        String mark = "";       // 演算記号
-        Short x = 0;            // 入力値
-        int sum = 0;            // 合計値
+        String mark = ""; // 演算記号
+        Short x = 0; // 入力値
+        int sum = 0; // 合計値
         for (int i = start; i < end; i++) {
-            System.out.println("i:" + i);
 
-            System.out.println("param:" + param[i]);
-            
-            if ("+".equals(param[i]) || "-".equals(param[i])) {       // 入力値が"+"か"-"の場合
+            if ("+".equals(param[i]) || "-".equals(param[i])) { // 入力値が"+"か"-"の場合
                 mark = param[i];
-            } else if (param[i].equals("(")) {                        // 入力値が"("の場合
-                int endi = endmark(param, i);       // ")"が入っている要素の添字
-                System.out.println("endi:" + endi);
+            } else if (param[i].equals("(")) { // 入力値が"("の場合
+                int endi = endmark(param, i); // ")"を見つける
 
-
-                switch (mark) {     //markによって計算
+                switch (mark) { // "("の前の記号によって計算し代入
                 case "+":
                     sum += calc(param, i + 1, endi);
                     break;
@@ -43,22 +38,21 @@ public class Question4 {
                     sum = calc(param, i + 1, endi);
                     break;
                 }
-                i = endi;           // ")"の次の添字を代入
-            } else if(param[i].equals(")")){        // 入力値に")"が来たら次へ
-                continue ;
-            } else {                    // 入力値が"+"か"-"以外
+                i = endi; // ")"の次へ
+            } else { // 入力値が"+"、"-"、"("以外の場合
                 try {
                     x = Short.parseShort(param[i]);
                 } catch (NumberFormatException e) {
-                    System.out.println("式を正しく入力してください");
+                    System.out.println("※式を正しく入力してください");
                     System.exit(0);
                 }
 
-                if (x < -10000 || x > 10000) {      // 入力値が-10000~10000でなければエラー
-                    System.out.println("-10000～10000で入力してください");
+                if (x < -10000 || x > 10000) { // 入力値が-10000~10000でなければエラー
+                    System.out.println("※-10000～10000で入力してください");
                     System.exit(0);
                 }
-                switch (mark) {         // 記号によって計算
+
+                switch (mark) { // 記号によって計算し代入
                 case "":
                     sum = x;
                     break;
@@ -70,23 +64,28 @@ public class Question4 {
                     break;
                 }
 
-                if (sum < -10000 || sum > 10000) {      // 計算途中で-10000～10000を越えたらエラー
+                if (sum < -10000 || sum > 10000) { // 計算途中で-10000～10000を越えたらエラー
                     System.out.println("※計算途中で-10000～10000の範囲外になりました");
                     System.exit(0);
                 }
             }
-            System.out.println("sum:" + sum);
         }
         return sum;
     }
 
-    //")"をさがす
+    // ")"をさがす
     public static int endmark(String[] param, int start) {
         int i = 0;
+        int count = 0;
 
         for (i = start; i < param.length; i++) {
-
-            if (")".equals(param[i])) {
+            if (param[i].equals("(")) {
+                count++;
+            }
+            if (param[i].equals(")")) {
+                count--;
+            }
+            if (count == 0) {
                 break;
             }
         }
