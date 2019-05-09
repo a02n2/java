e8ee62d588a26bbc171c3bbd84610d071c73d0b9
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-05-09 15:55:06 +0900

    再帰
    コメント、メソッド名の修正
    switch文が重複していたので別メソッドへ
    (が続いたときエラー表示
    equalsの書き方を統一

--- Question4.java
+++ Question4.java
@@ -15,34 +15,35 @@ public class Question4 {
         System.out.println(sum);
     }
 
+    // 入力された式の計算
     public static int calc(String[] param, int start, int end) {
 
         String mark = ""; // 演算記号
-        Short x = 0; // 入力値
-        int sum = 0; // 合計値
+        int x = 0; // 入力値
+        int sum = 0; // 合計値 
+
         for (int i = start; i < end; i++) {
 
-            if ("+".equals(param[i]) || "-".equals(param[i])) { // 入力値が"+"か"-"の場合
+            if (param[i].equals("+") || param[i].equals("-")) { // 入力値が"+"か"-"の場合
                 mark = param[i];
-            } else if (param[i].equals("(")) { // 入力値が"("の場合
-                int endi = endmark(param, i); // ")"を見つける
-
-                switch (mark) { // "("の前の記号によって計算し代入
-                case "+":
-                    sum += calc(param, i + 1, endi);
-                    break;
-                case "-":
-                    sum -= calc(param, i + 1, endi);
-                    break;
-                case "":
-                    sum = calc(param, i + 1, endi);
-                    break;
+            } else if (param[i].equals("(")) { //入力値が"("の場合
+                int rightParen = 0;     //")"を格納
+                
+                try{
+                    rightParen = searchRightParen(param, i); // ")"を見つける
+                }catch(Exception e){        // "()"の数がおかしければエラー
+                    System.out.println("()を正しく入力してください");
+                    System.exit(0);
                 }
-                i = endi; // ")"の次へ
+
+                // "()"内を計算
+                sum = calc1(mark, sum, calc(param, i + 1, rightParen));
+
+                i = rightParen; // ")"の次からループ再開    
             } else { // 入力値が"+"、"-"、"("以外の場合
                 try {
-                    x = Short.parseShort(param[i]);
-                } catch (NumberFormatException e) {
+                    x = Integer.parseInt(param[i]);
+                } catch (NumberFormatException e) {     //入力された式がおかしければエラー
                     System.out.println("※式を正しく入力してください");
                     System.exit(0);
                 }
@@ -52,43 +53,57 @@ public class Question4 {
                     System.exit(0);
                 }
 
-                switch (mark) { // 記号によって計算し代入
-                case "":
-                    sum = x;
-                    break;
-                case "+":
-                    sum += x;
-                    break;
-                case "-":
-                    sum -= x;
-                    break;
-                }
+                // 合計値の計算
+                sum = calc1(mark, sum, x);
 
-                if (sum < -10000 || sum > 10000) { // 計算途中で-10000～10000を越えたらエラー
-                    System.out.println("※計算途中で-10000～10000の範囲外になりました");
-                    System.exit(0);
-                }
             }
         }
         return sum;
     }
 
-    // ")"をさがす
-    public static int endmark(String[] param, int start) {
-        int i = 0;
-        int count = 0;
+    // 値の加算、減算
+    public static int calc1(String mark, int sum, int x) {
 
-        for (i = start; i < param.length; i++) {
-            if (param[i].equals("(")) {
-                count++;
+        // 演算記号によって計算しsumに代入
+        switch (mark) {
+        case "":
+            sum = x;
+            break;
+        case "+":
+            sum += x;
+            break;
+        case "-":
+            sum -= x;
+            break;
+        }
+
+        if (sum < -10000 || sum > 10000) { // 計算途中で-10000～10000を越えたらエラー
+            System.out.println("※計算途中で-10000～10000の範囲外になりました");
+            System.exit(0);
+        }
+
+        return sum;
+    }
+
+    // ")"をさがす
+    public static int searchRightParen(String[] param, int leftparan) throws Exception{
+
+        int i = leftparan;
+        int parencount = 0; // "()"の数をカウント
+
+        // "("から式の最後までループし")"を探す
+        for (; i < param.length; i++) {
+            if (param[i].equals("(")) { // "("がでてきたらcount+1
+                parencount++;
+            } else if (param[i].equals(")")) { // ")"がでてきたらcount-1
+                parencount--;
             }
-            if (param[i].equals(")")) {
-                count--;
-            }
-            if (count == 0) {
-                break;
+
+            if (parencount == 0) { // countが0(=対応する")"をみつける)とreturn
+                return i;
             }
         }
-        return i;
+
+        throw new Exception();    //countが0でなければ例外発生
     }
 }
