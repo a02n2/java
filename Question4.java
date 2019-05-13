dc99ee88e58617a5c269e9af07cece9489987b5f
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-05-13 11:14:08 +0900

    Update Question4.java
    roopをloopCalcに変更
    ドキュメンテーションコメント変更
    calcで終了していたエラーを例外を投げmainで処理

--- Question4.java
+++ Question4.java
@@ -11,19 +11,21 @@ public class Question4 {
         System.out.println("※複数回の演算も行えます");
         String input = br.readLine();
         String[] param = input.split(" ");
-        int sum = roop(param, 0, param.length);
+        int sum = loopCalc(param, 0, param.length);
         System.out.println(sum);
     }
 
     /**
-     * 入力された式を始めから終わりまでループします。
-     * 計算を行う別メソッドを呼び出し、最終の合計値を返します。
+     * 変数paramに格納されている計算式を変数startからカウンターが変数endに達するまでループ
+     * 変数paramから演算記号、数値を取得
+     * ()が存在すれば再帰呼び出しを行い、先に()内の処理をする
+     * 加算減算の処理はcalcを呼び出す
      * @param param 入力された式
      * @param start 式の始まり
      * @param end 式の終わり
-     * @return 最終の合計値
+     * @return 合計値
      */
-    public static int roop(String[] param, int start, int end) {
+    public static int loopCalc(String[] param, int start, int end) {
 
         String mark = "";   // 演算記号
         int x = 0;          // 入力値
@@ -38,13 +40,18 @@ public class Question4 {
 
                 try {
                     closeParen = searchCloseParen(param, i);    // ")"を見つける
-                } catch (Exception e) {     // "()"の数がおかしければエラー
+                } catch (Exception e) {          // "()"の数がおかしければエラー
                     System.out.println("()を正しく入力してください");
                     System.exit(0);
                 }
 
                 // "()"内の合計値
-                sum = calc(mark, sum, roop(param, i + 1, closeParen));
+                try{
+                    sum = calc(mark, sum, loopCalc(param, i + 1, closeParen));
+                }catch(Exception e){            //-10000～10000を超えるとエラー
+                    System.out.println("※-10000～10000の範囲外になりました");
+                    System.exit(0);
+                }
 
                 i = closeParen;    // ")"の次からループ再開    
             } else { 
@@ -56,21 +63,26 @@ public class Question4 {
                 }
 
                 // 合計値
-                sum = calc(mark, sum, x);
-
+                try{
+                    sum = calc(mark, sum, x);
+                }catch(Exception e){            //-10000～10000を超えるとエラー
+                    System.out.println("※-10000～10000の範囲外になりました");
+                    System.exit(0);
+                }
             }
         }
         return sum;
     }
 
     /**
-     * 計算途中の合計値に数値を加算または減算します。
+     * 計算途中の合計値に数値を加算または減算
      * @param mark 入力された演算記号
      * @param sum 計算途中の合計値
      * @param x　入力された数値
      * @return　現在の合計値に数値を加算または減算した結果
+     * @throws Exception -10000から10000を超えた時
      */
-    public static int calc(String mark, int sum, int x) {
+    public static int calc(String mark, int sum, int x)throws Exception {
 
         // 演算記号によって計算しsumに代入
         switch (mark) {
@@ -85,16 +97,15 @@ public class Question4 {
             break;
         }
 
-        if (sum < -10000 || sum > 10000) {      // -10000～10000を越えたらエラー
-            System.out.println("※-10000～10000の範囲外になりました");
-            System.exit(0);
+        if (sum < -10000 || sum > 10000) {      // -10000～10000を越えたら例外発生
+           throw new Exception();
         }
 
         return sum;
     }
 
     /**
-     * 開始括弧に対応する終了括弧を探します。
+     * 開始括弧に対応する終了括弧を探す
      * @param param 入力された式
      * @param openParen 開始括弧
      * @return 終了括弧
