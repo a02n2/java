f45439e68f6135b58233d3323b8d4f2bbc656f9b
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-05-13 09:16:52 +0900

    再帰
    parenのright,leftをopen,closeに変更
    calc1をcalcに変更
    ドキュメンテーションコメントを追加
    コメントを少なく
    エラーが重複していたので片方を削除

--- Question4.java
+++ Question4.java
@@ -11,58 +11,66 @@ public class Question4 {
         System.out.println("※複数回の演算も行えます");
         String input = br.readLine();
         String[] param = input.split(" ");
-        int sum = calc(param, 0, param.length);
+        int sum = roop(param, 0, param.length);
         System.out.println(sum);
     }
 
-    // 入力された式の計算
-    public static int calc(String[] param, int start, int end) {
+    /**
+     * 入力された式を始めから終わりまでループします。
+     * 計算を行う別メソッドを呼び出し、最終の合計値を返します。
+     * @param param 入力された式
+     * @param start 式の始まり
+     * @param end 式の終わり
+     * @return 最終の合計値
+     */
+    public static int roop(String[] param, int start, int end) {
 
-        String mark = ""; // 演算記号
-        int x = 0; // 入力値
-        int sum = 0; // 合計値 
+        String mark = "";   // 演算記号
+        int x = 0;          // 入力値
+        int sum = 0;        // 合計値 
 
         for (int i = start; i < end; i++) {
 
-            if (param[i].equals("+") || param[i].equals("-")) { // 入力値が"+"か"-"の場合
+            if (param[i].equals("+") || param[i].equals("-")) { 
                 mark = param[i];
-            } else if (param[i].equals("(")) { // 入力値が"("の場合
-                int rightParen = 0; // ")"
+            } else if (param[i].equals("(")) { 
+                int closeParen = 0; 
 
                 try {
-                    rightParen = searchRightParen(param, i); // ")"を見つける
-                } catch (Exception e) { // "()"の数がおかしければエラー
+                    closeParen = searchCloseParen(param, i);    // ")"を見つける
+                } catch (Exception e) {     // "()"の数がおかしければエラー
                     System.out.println("()を正しく入力してください");
                     System.exit(0);
                 }
 
-                // "()"内を計算
-                sum = calc1(mark, sum, calc(param, i + 1, rightParen));
+                // "()"内の合計値
+                sum = calc(mark, sum, roop(param, i + 1, closeParen));
 
-                i = rightParen; // ")"の次からループ再開    
-            } else { // 入力値が"+"、"-"、"("以外の場合
+                i = closeParen;    // ")"の次からループ再開    
+            } else { 
                 try {
                     x = Integer.parseInt(param[i]);
-                } catch (NumberFormatException e) { // 入力された式がおかしければエラー
+                } catch (NumberFormatException e) {     // 入力された式がおかしければエラー
                     System.out.println("※式を正しく入力してください");
                     System.exit(0);
                 }
 
-                if (x < -10000 || x > 10000) { // 入力値が-10000~10000でなければエラー
-                    System.out.println("※-10000～10000で入力してください");
-                    System.exit(0);
-                }
-
-                // 合計値の計算
-                sum = calc1(mark, sum, x);
+                // 合計値
+                sum = calc(mark, sum, x);
 
             }
         }
         return sum;
     }
 
-    // 値の加算、減算
-    public static int calc1(String mark, int sum, int x) {
+    /**
+     * 計算途中の合計値に数値を加算または減算します。
+     * @param mark 入力された演算記号
+     * @param sum 計算途中の合計値
+     * @param x　入力された数値
+     * @return　現在の合計値に数値を加算または減算した結果
+     */
+    public static int calc(String mark, int sum, int x) {
 
         // 演算記号によって計算しsumに代入
         switch (mark) {
@@ -77,29 +85,35 @@ public class Question4 {
             break;
         }
 
-        if (sum < -10000 || sum > 10000) { // 計算途中で-10000～10000を越えたらエラー
-            System.out.println("※計算途中で-10000～10000の範囲外になりました");
+        if (sum < -10000 || sum > 10000) {      // -10000～10000を越えたらエラー
+            System.out.println("※-10000～10000の範囲外になりました");
             System.exit(0);
         }
 
         return sum;
     }
 
-    // ")"をさがす
-    public static int searchRightParen(String[] param, int leftparan) throws Exception {
+    /**
+     * 開始括弧に対応する終了括弧を探します。
+     * @param param 入力された式
+     * @param openParen 開始括弧
+     * @return 終了括弧
+     * @throws Exception ()の数が合わない時
+     */
+    public static int searchCloseParen(String[] param, int openParen) throws Exception {
 
-        int i = leftparan;
-        int parencount = 0; // "()"の数をカウント
+        int i = openParen;
+        int parenCount = 0; // "()"の数をカウント
 
-        // "("から式の最後までループし")"を探す
+        // "("から式の最後までループし、対応する")"を見つける(countが0になる)とreturn
         for (; i < param.length; i++) {
-            if (param[i].equals("(")) { // "("がでてきたらcount+1
-                parencount++;
-            } else if (param[i].equals(")")) { // ")"がでてきたらcount-1
-                parencount--;
+            if (param[i].equals("(")) { 
+                parenCount++;
+            } else if (param[i].equals(")")) { 
+                parenCount--;
             }
 
-            if (parencount == 0) { // countが0(=対応する")"をみつける)とreturn
+            if (parenCount == 0) { 
                 return i;
             }
         }
