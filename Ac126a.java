d8de4e89a833f089beb837db46c9e39c41ae24cb
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-05-23 16:05:42 +0900

    AtCoder126.A
    int型への変換をメソッド化
    制約のチェックをメソッド化
    ！で書いていた条件式を！を外して書く
    [A-C]*を[A-C]+に変更

--- Ac126a.java
+++ Ac126a.java
@@ -9,39 +9,70 @@ public class Ac126a {
         System.out.println("一行目に文字列の長さ、小文字にする位置を空白で区切って入力して下さい");
         System.out.println("二行目にABCからできた文字列を入力してください");
         String str = br.readLine();
-        String s = br.readLine();
-        String[] nk = str.split(" ", 2);
+        String abcStr = br.readLine();
+        String[] lenLow = str.split(" ", 2);
 
+        // 文字列の長さと小文字にする位置をint型に変換
+        int length = convert(lenLow[0], "文字列の長さ");
+        int lowerCase = convert(lenLow[1], "小文字にする位置");
+
+        // 　制約チェック
+        check(length, lowerCase, abcStr);
+
+        // 文字列の指定の位置を小文字に変換し、出力
+        abcStr = abcStr.substring(0, lowerCase - 1) + abcStr.substring(lowerCase - 1, lowerCase).toLowerCase()
+                + abcStr.substring(lowerCase);
+        System.out.println(abcStr);
+    }
+
+    /**
+     * String型で入力された文字列の長さと小文字にする位置をint型に変換
+     * 
+     * @param strLenLow　文字列の長さ、小文字にする位置
+     * @param text　"文字列の長さ"、"小文字にする位置"の文章
+     * @return　変換した値
+     */
+    public static int convert(String strLenLow, String text) {
+        int lenLow = 0;
         try {
-            int n = Integer.parseInt(nk[0]);
-            int k = Integer.parseInt(nk[1]);
-
-            if (!(n >= 1 && n <= 50)) {
-                System.out.println("文字列の長さが範囲外です");
-                System.exit(0);
-            }
-
-            if (!(k >= 1 && k <= n)) {
-                System.out.println("小文字に変換する位置が範囲外です");
-                System.exit(0);
-            }
-
-            if (!(n == s.length())) {
-                System.out.println("指定した文字列の長さと、ABCからできた文字列の長さが違います");
-                System.exit(0);
-            }
-
-            if (!s.matches("[A-C]*")) {
-                System.out.println("文字列はABCだけで作ってください");
-                System.exit(0);
-            }
-
-            // 文字列の指定の位置を小文字に変換し、出力
-            s = s.substring(0, k - 1) + s.substring(k - 1, k).toLowerCase() + s.substring(k);
-            System.out.println(s);
-
+            lenLow = Integer.parseInt(strLenLow);
         } catch (NumberFormatException e) {
-            System.out.println("正しく入力してください");
+            System.out.println(text + "が正しく指定されていません");
+            System.exit(0);
+        }
+        return lenLow;
+    }
+
+    /**
+     * 制約のチェックを行う 
+     * ・1<=文字列の長さ<=50 
+     * ・1<=小文字にする位置<=文字列の長さ
+     * ・文字列はABCからなる
+     * 
+     * @param length    文字列の長さ
+     * @param lowerCase 小文字にする位置
+     * @param abcStr    文字列
+     */
+    public static void check(int length, int lowerCase, String abcStr) {
+
+        if (length < 1 || length > 50) {
+            System.out.println("文字列の長さが範囲外です");
+            System.exit(0);
+        }
+
+        if (lowerCase < 1 || lowerCase > length) {
+            System.out.println("小文字に変換する位置が範囲外です");
+            System.exit(0);
+        }
+
+        if (length != abcStr.length()) {
+            System.out.println("指定した文字列の長さと、ABCからできた文字列の長さが違います");
+            System.exit(0);
+        }
+
+        if (!abcStr.matches("[A-C]+")) {
+            System.out.println("文字列はABCだけで作ってください");
+            System.exit(0);
         }
     }
 }
