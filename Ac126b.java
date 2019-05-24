89b8a8501a41ebd7a92d93d03b515ec01aac28c3
a02n2 <49223782+a02n2@users.noreply.github.com>
2019-05-24 10:42:10 +0900

    YYMM,MMYYフォーマット

---
+++ Ac126b.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Ac126b {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("4文字の数字列を入力してください");
+        String str = br.readLine();
+
+        if (str.length() != 4) {
+            System.out.println("4文字で入力してください");
+            System.exit(0);
+        }
+
+        // 前2文字と後ろ2文字で分ける
+        String front_str = str.substring(0, 2);
+        String back_str = str.substring(2, 4);
+
+        // String型からint型へ変換
+        int front = 0;
+        int back = 0;
+        try {
+            front = Integer.parseInt(front_str);
+            back = Integer.parseInt(back_str);
+        } catch (NumberFormatException e) {
+            System.out.println("数値が正しく入力されていません");
+            System.exit(0);
+        }
+
+        boolean my = (front >= 1 && front <= 12);
+        boolean ym = (back >= 1 && back <= 12);
+
+        // myがtrue→MMYY、ymがtrue→YYMM、両方true→AMBIGUOS,両方false→NA、を表示
+        if (my) {
+            if (ym) {
+                System.out.println("AMBIGUOS");
+            } else {
+                System.out.println("MMYY");
+            }
+        } else if (ym) {
+            System.out.println("YYMM");
+        } else {
+            System.out.println("NA");
+        }
+    }
+}
